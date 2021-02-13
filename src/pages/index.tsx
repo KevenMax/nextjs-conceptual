38bc019e01f70bbe30b5f4ae23b723560b227832
Keven Max <kmax136@gmail.com>
2021-02-12 23:38:53 -0300

    refactor: Add call to API on react lifecycle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,34 @@
-import * as Styled from '../styles/pages/Home';
+import { useEffect, useState } from 'react'
+import * as Styled from '../styles/pages/Home'
+
+interface IProduct {
+  id: string;
+  title: string;
+}
 
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3002/recommended').then(response => {
+      response.json().then(data => {
+        setRecommendedProducts(data)
+      })
+    })
+  }, [])
   return (
-    <div>
-      <Styled.Title>Hello World!</Styled.Title>
-    </div>
+      <Styled.Section>
+        <Styled.Title>Hello World!</Styled.Title>
+
+        <Styled.List>
+          {recommendedProducts.map((recommendedProduct) => {
+            return (
+              <Styled.Item key={recommendedProduct.id}>
+                {recommendedProduct.title}
+              </Styled.Item>
+            )
+          })}
+        </Styled.List>
+      </Styled.Section>
   )
 }
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components'
 
+export const Section = styled.section``;
+
 export const Title = styled.h1`
   color: #8257e5;
 `;
+
+export const List = styled.ul``;
+
+export const Item = styled.li``;
