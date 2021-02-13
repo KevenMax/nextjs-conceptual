f7cac2bd2851bcc502ed45cb2bdeccd9bc6f807c
Keven Max <kmax136@gmail.com>
2021-02-13 00:53:26 -0300

    feat: Use server side generation with params dynamic

---
+++ src/pages/catalog/categories/[slug].tsx
@@ -0,0 +1,66 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { useRouter } from 'next/router'
+
+interface ICategory {
+  id: string;
+  title: string;
+}
+
+interface IProduct {
+  id: string;
+  title: string;
+}
+
+interface CategoryProps {
+  products: IProduct[];
+}
+
+export default function Product({products}: CategoryProps) {
+  const router = useRouter()
+  
+  return (
+    <div>
+      <h1>{router.query.slug}</h1>
+
+      <ul>
+        {products.map((product) => {
+          return (
+            <li key={product.id}>
+              {product.title}
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const response = await fetch(`http://localhost:3002/categories`)
+  const categories = await response.json()
+
+  const paths = categories.map((category: ICategory) => {
+    return {
+      params: { slug: category.id }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps<CategoryProps> = async (context) => {
+  const { slug } = context.params;
+
+  const response = await fetch(`http://localhost:3002/products?category_id=${slug}`)
+  const products = await response.json()
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 60,
+  }
+}
