f4e45f212af9437e13167d9f72f4d5c62ac88e2b
Keven Max <kmax136@gmail.com>
2021-02-12 23:22:00 -0300

    feat: Add styled components with server-side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import * as Styled from '../styles/pages/Home';
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Styled.Title>Hello World!</Styled.Title>
     </div>
   )
 }
