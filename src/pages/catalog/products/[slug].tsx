f2435b68123ef08a6ca1d45d7ce608b2c4ab1dcc
Keven Max <kmax136@gmail.com>
2021-02-13 02:07:12 -0300

    feat: Dynamic imports

--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -1,8 +1,26 @@
+import { useState } from 'react'
+import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router'
+
+const AddToCartModal = dynamic(
+  () => import('../../../components/AddToCartModal'), 
+  { loading: () => <p>Loading...</p> }
+)
 
 export default function Product() {
   const router = useRouter()
+  const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false)
+  
+  const handleAddToCart = () => {
+    setIsAddToCartModalVisible(true);
+  }
+
   return (
-    <h1>{router.query.slug}</h1>
+    <div>
+      <h1>{router.query.slug}</h1>
+  
+      <button onClick={handleAddToCart}>Add to cart</button>
+      {isAddToCartModalVisible && <AddToCartModal />}
+    </div>
   )
 }
